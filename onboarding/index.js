d4cfd2e8187645091b349755d300ec4017ee1931
Sengulair <vsevolod.fedorov15@gmail.com>
2024-01-07 16:16:43 +0200

    chore: renaming

--- onboarding/index.js
+++ onboarding/index.js
@@ -14,26 +14,26 @@ const GOOGLE_RECAPTCHA_SECRET_KEY = process.env.GOOGLE_RECAPTCHA_SECRET_KEY;
 const APP_PORT = process.env.APP_PORT;
 const EXPIRATION_TIMEOUT_MS = 10 * 60 * 1000; // 10 minutes
 
-const app = Fastify();
+const fastify = Fastify({ logger: false });
 
 // Memory stores
 const dataStore = new Map();
 const clients = new Map();
 
-app.register(rateLimit, {
+fastify.register(rateLimit, {
   max: 5, // limit each IP to 5 requests per windowMs
   timeWindow: '1 minute',
 });
 
-app.register(fastifyStatic, {
+fastify.register(fastifyStatic, {
   root: path.join(__dirname, 'public'),
 });
 
-app.get('/api/config', (req, reply) => {
+fastify.get('/api/config', (req, reply) => {
   return { recaptchaSiteKey: process.env.GOOGLE_RECAPTCHA_SITE_KEY };
 });
 
-app.post('/api/data', async (req, reply) => {
+fastify.post('/api/data', async (req, reply) => {
   const { recaptcha, data } = req.body;
 
   const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
@@ -56,7 +56,7 @@ app.post('/api/data', async (req, reply) => {
   return { message: 'Data added successfully' };
 });
 
-app.get('/api/data/:code', async (req, reply) => {
+fastify.get('/api/data/:code', async (req, reply) => {
   const code = req.params.code;
   const data = dataStore.get(code);
 
@@ -76,7 +76,7 @@ app.get('/api/data/:code', async (req, reply) => {
   return data;
 });
 
-app.get('/events/:code', (req, reply) => {
+fastify.get('/events/:code', (req, reply) => {
   reply.raw.setHeader('Cache-Control', 'no-cache');
   reply.raw.setHeader('Content-Type', 'text/event-stream');
   reply.raw.setHeader('Connection', 'keep-alive');
@@ -95,4 +95,10 @@ app.get('/events/:code', (req, reply) => {
   });
 });
 
-app.listen({ port: APP_PORT, host: '0.0.0.0' }, () => console.log(`Server is listening on port ${APP_PORT}`));
+fastify.listen({ port: APP_PORT, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } 
+  console.log(`Server is listening on ${address}`)
+});
